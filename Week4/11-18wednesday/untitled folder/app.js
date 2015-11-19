ceda4a04cbe98282b987615336bc560b06c1542d
James Shieldsclear <James80401@gmail.com>
2015-11-19 10:34:29 -0700

    Thrusday update

--- Week4/11-18wednesday/untitled folder/app.js
+++ Week4/11-18wednesday/untitled folder/app.js
@@ -11,29 +11,54 @@ $(document).on('ready', function(){
   // stateInfo.on('click', function(){
   //   console.log(stateInfo.val())
   // });
+  // saveSearchData();
     button.on('click', function(){
       event.preventDefault();
-      var zipCode =
+      var zipCode = {};
+      var distance = {};
       $.ajax({
               url: 'https://www.zipcodeapi.com/rest/js-CV19L6yxnrcTPBpwGgZhT8UyrlwLONiqJkzuq72OzeIoQ7UxoTOh2JQu4nfTbyjU/city-zips.json/'+cityInfo.val()+'/'+stateInfo.val(),
               //my api key <js-CV19L6yxnrcTPBpwGgZhT8UyrlwLONiqJkzuq72OzeIoQ7UxoTOh2JQu4nfTbyjU>
               method: "GET",
               success: function(data) {
-                alert(JSON.stringify(data));
-              }
+                zipCode = data.zip_codes[2];
+                console.log(zipCode);
+                // alert(JSON.stringify(data));
+
+              var distance = {};
+              $.ajax({
+                url:'https://www.zipcodeapi.com/rest/js-OQMTMvUyxu5nfS2bUK8cB5BDtQmhuXFjNrcAW68uBeRR6T21DaefgPTJGf1adGt0/distance.json/80524/'+zipCode+'/mile',
+                //0Q4PWI5bkS1pL5zA9w59oL0dvau2vABvqFlnoqzJiA84GHFQeL0Cw0Q1bk4HVFcm
+                //js-OQMTMvUyxu5nfS2bUK8cB5BDtQmhuXFjNrcAW68uBeRR6T21DaefgPTJGf1adGt0
+                method:'GET',
+                success: function(data) {
+                  distance = data;
+                  console.log(distance)
+                }
+              })
+            }
             });
-console.log()
-        //   $.ajax({
-        // url: 'http://https://www.zipcodeapi.com/rest/xrJtnT1RxLF2hzlVQfPJemftaycRmvFTXonliodQaSSrrdnitd1HzhbKQoBcUh6R/distance.json/<zip_code1>/80524/mile',
-        // //my api key <xrJtnT1RxLF2hzlVQfPJemftaycRmvFTXonliodQaSSrrdnitd1HzhbKQoBcUh6R>
-        // method: "GET",
-        // success: function(data) {
-        //   alert(JSON.stringify(data));
-        // }
+            // console.log(zipCode.responseText)
+      //     $.ajax({
+      //   url: 'https://www.zipcodeapi.com/rest/NKYTaNaRyi7dYJCrMEnzQ5e7GHy9f3c2NaWYY6QxpXKoGnQs1e2SkgZ1XxacBmfo/distance.json/80524/'+zipCode+'/mile',
+      //   //my api key <xrJtnT1RxLF2hzlVQfPJemftaycRmvFTXonliodQaSSrrdnitd1HzhbKQoBcUh6R>
+      //   method: "GET",
+      //   success: function(data) {
+      //     alert(JSON.stringify(data));
+      //   }
       // });
     });
 });
-
+//save input fields into local storage for users to redo the process.
+//we should try to confirm if what we want is already in storeage.
+                //on page load
+                //
+                // funciton saveSearchData(){
+                //   localStorage.setItem('city')
+                // };
+                // funciton readSearchData(){
+                //   localStorage.setItem('state')
+                // };
 //
 //
 //       httpRequest = new XMLHttpRequest();
